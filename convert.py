6976de9dcd8e3d2f6ce2a7b48194a308a391f814
zisianw <zisian94@gmail.com>
2018-10-26 13:57:30 +0800

    update xml

--- convert.py
+++ convert.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
 from PIL import Image
 import os
+import argparse
+import sys
 
 def createAnnotationPascalVocTree(folder, basename, path, width, height):
     annotation = ET.Element('annotation')
     ET.SubElement(annotation, 'folder').text = folder
     ET.SubElement(annotation, 'filename').text = basename
-    ET.SubElement(annotation, 'path').text = path
+    #ET.SubElement(annotation, 'path').text = path
 
     source = ET.SubElement(annotation, 'source')
     ET.SubElement(source, 'database').text = 'Unknown'
@@ -22,7 +23,7 @@ def createAnnotationPascalVocTree(folder, basename, path, width, height):
 
     return ET.ElementTree(annotation)
 
-def createObjectPascalVocTree(xmin, ymin, xmax, ymax):
+def createObjectPascalVocTree(xmin, ymin, xmax, ymax, image_width, image_height):
     obj = ET.Element('object')
     ET.SubElement(obj, 'name').text = 'face'
     ET.SubElement(obj, 'pose').text = 'Unspecified'
@@ -30,10 +31,10 @@ def createObjectPascalVocTree(xmin, ymin, xmax, ymax):
     ET.SubElement(obj, 'difficult').text = '0'
 
     bndbox = ET.SubElement(obj, 'bndbox')
-    ET.SubElement(bndbox, 'xmin').text = xmin
-    ET.SubElement(bndbox, 'ymin').text = ymin
-    ET.SubElement(bndbox, 'xmax').text = xmax
-    ET.SubElement(bndbox, 'ymax').text = ymax
+    ET.SubElement(bndbox, 'xmin').text = str(max(xmin,0))
+    ET.SubElement(bndbox, 'ymin').text = str(max(ymin,0))
+    ET.SubElement(bndbox, 'xmax').text = str(min(xmax,image_width))
+    ET.SubElement(bndbox, 'ymax').text = str(min(ymax,image_height))
 
     return ET.ElementTree(obj)
 
@@ -49,39 +50,50 @@ def convertWFAnnotations(annotationsPath, targetPath, imPath):
     ann = None
     basename = ''
     with open(annotationsPath) as f:
-        while True:
-            imFilename = f.readline().strip()
-            if imFilename:
-                folder, basename, path, width, height = parseImFilename(imFilename, imPath)
-                ann = createAnnotationPascalVocTree(folder, basename, os.path.join(imPath, path), width, height)
-                nbBndboxes = f.readline()
-                
-                i = 0
-                while i < int(nbBndboxes):
-                    i = i + 1
-                    x1, y1, w, h, _, _, _, _, _, _ = [int(i) for i in f.readline().split()]
+        line = f.readline().strip()
+        while line:
+            imFilename = line
+            folder, basename, path, width, height = parseImFilename(imFilename, imPath)
+            ann = createAnnotationPascalVocTree(folder, basename, os.path.join(imPath, path), width, height)
+            nbBndboxes = f.readline().strip()
+            
+            i = 0
+            while i < int(nbBndboxes):
+                i = i + 1
+                x1, y1, w, h, _, _, _, _, _, _ = [int(k) for k in f.readline().split()]
+                x2 = x1 + w
+                y2 = y1 + h
 
-                    ann.getroot().append(createObjectPascalVocTree(str(x1), str(y1), str(x1 + w), str(y1 + h)).getroot())
-                
-                if not os.path.exists(targetPath):
-                     os.makedirs(targetPath)
-                annFilename = os.path.join(targetPath, basename.replace('.jpg','.xml'))
-                ann.write(annFilename)
-                print('{} => {}'.format(basename, annFilename))
-            else:
-                break 
-    f.close()
+                if (x2 <= x1 or y2 <= y1):
+                    print('Error Image "{}" x1 y1 w h: {} {} {} {}'.format(imFilename,x1,y1,w,h))
+                    continue
 
+                ann.getroot().append(createObjectPascalVocTree(x1, y1, x2, y2, width, height).getroot())
+
+            xmlstr = minidom.parseString(ET.tostring(ann.getroot())).childNodes[0].toprettyxml(indent="    ")
+            if not os.path.exists(targetPath):
+                 os.makedirs(targetPath)
+            annFilename = os.path.join(targetPath, basename.replace('.jpg','.xml'))
+            with open(annFilename,"w") as sf:
+                sf.write(xmlstr)
+            print('{} => {}'.format(basename, annFilename))
+            line = f.readline().strip()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert WIDER annotations to VOC format')
+    parser.add_argument('-ap', dest='annotations_path', help='the annotations file path. ie:"./wider_face_split/wider_face_train_bbx_gt.txt".')
+    parser.add_argument('-tp', dest='target_path', help='the target directory path where XML files will be copied.')
+    parser.add_argument('-ip', dest='images_path', help='the images directory path. ie:"./WIDER_train/images"')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
-    import argparse
 
-    PARSER = argparse.ArgumentParser()
-    PARSER.add_argument('-ap', '--annotations-path', help='the annotations file path. ie:"./wider_face_split/wider_face_train_bbx_gt.txt".')
-    PARSER.add_argument('-tp', '--target-path', help='the target directory path where XML files will be copied.')
-    PARSER.add_argument('-ip', '--images-path', help='the images directory path. ie:"./WIDER_train/images"')
-
-    ARGS = vars(PARSER.parse_args())
+    args = parse_args()
     
-    convertWFAnnotations(ARGS['annotations_path'], ARGS['target_path'], ARGS['images_path'])
+    convertWFAnnotations(args.annotations_path, args.target_path, args.images_path)
 
